6d2d56ab2adaeb013f924b1a48e4207e98527e18
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2019-09-23 05:58:32 +0200

    lifecycle hooks

---
+++ src/main/java/learnjsf/SampleViewScopedBean.java
@@ -0,0 +1,44 @@
+package learnjsf;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named("sampleViewBean")
+@ViewScoped
+public class SampleViewScopedBean implements Serializable {
+
+    private List<String> dogs;
+
+    public List<String> getDogs() {
+        return dogs;
+    }
+
+    public void setDogs(List<String> dogs) {
+        this.dogs = dogs;
+    }
+
+    @PostConstruct
+    public void initDogs() {
+        dogs = new ArrayList<>();
+        dogs.add("corgi");
+        dogs.add("retriever");
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        dogs.clear();
+    }
+
+    public void getSession() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        // logout or page timeout
+        session.invalidate();
+    }
+}
