66209f39ae653e66909ec07daecff25e473b3c0b
Sebastian Gulczynski <sebastian.gulczynski@capgemini.com>
2019-09-23 10:56:10 +0200

    port modification in bat for running in windows

--- buildAndRun.bat
+++ buildAndRun.bat
@@ -2,4 +2,4 @@
 call mvn clean package
 call docker build -t com.gulci/learnjsf .
 call docker rm -f learnjsf
-call docker run -d -p 8080:8080 -p 4848:4848 --name learnjsf com.gulci/learnjsf
+call docker run -d -p 9090:8080 -p 4848:4848 --name learnjsf com.gulci/learnjsf
